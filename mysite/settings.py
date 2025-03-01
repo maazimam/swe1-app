29f2b2f2b5bedd1f2056297d304a76207b8e73c5
Maaz Imam <syedmaazimam24@gmail.com>
2025-02-28 22:09:34 -0500

    Fix database TypeError in Django settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -76,9 +76,9 @@ WSGI_APPLICATION = "mysite.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': str(BASE_DIR / "db.sqlite3"),  # Convert Path object to string
     }
 }
 
